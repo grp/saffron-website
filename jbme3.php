1aee4eca96198c6fad3001c1373568554038a459
root <chpwn@chpwn.com>
2011-07-05 02:32:34 +0000

    basic, mostly working firefox support

--- jbme3.php
+++ jbme3.php
@@ -132,11 +132,13 @@ li {
     line-height: 16px; 
     
     -webkit-box-shadow: 0 -1px 0px #79797b, 0 1px 0px #ffffff;
+    -moz-box-shadow: 0 -1px 0px #79797b, 0 1px 0px #ffffff;
     border-top: 1px solid #505050;
     
     padding-top: 1px;
     height: 20px;
     -webkit-border-radius: 3px;
+    -moz-border-radius: 3px;
     
     width: 48px;
     text-align: center;
@@ -145,6 +147,7 @@ li {
     position: relative;
 <?php if($small_device) { ?>
     -webkit-transform-origin: right 50%;
+    -moz-transform-origin: right 50%;
 <?php } else { ?>
     margin-top: 7px;
 <?php } ?>
@@ -153,6 +156,7 @@ li {
 <?php } ?>
     
     -webkit-transform: scaleX(1.001);
+    -moz-transform: scaleX(1.001);
 }
 
 .button-shadow {
@@ -197,6 +201,10 @@ li {
         color-stop(0.95, #60749b),
         color-stop(0, #32559c)
     );
+    background-image: -moz-linear-gradient(
+        #60749b,
+        #32559c
+    );
 <?php } else { ?>
     background-image: -webkit-gradient(
         linear,
@@ -205,6 +213,10 @@ li {
         color-stop(1, #74767a),
         color-stop(0.95, #8d8f96),
         color-stop(0, #75777f)
+    );
+    background-image: -moz-linear-gradient(
+        #8d8f96,
+        #75777f
     );
 <?php } ?>
 }
@@ -222,6 +234,10 @@ li {
         color-stop(0.95, #74a361),
         color-stop(0, #51ab29)
     );
+    background-image: -moz-linear-gradient(
+        #74a361,
+        #51ab29
+    );
 
     width: 68px;
     margin-left: -10px;
@@ -229,16 +245,21 @@ li {
 
 .button-animated .button {
     -webkit-transition-property: -webkit-transform, background-image, border-color;
+    -moz-transition-property: -webkit-transform, background-image, border-color;
     -webkit-transition-timing-function: ease-in-out;
+    -moz-transition-timing-function: ease-in-out;
     -webkit-transition-duration: 0.2s;
+    -moz-transition-duration: 0.2s;
 }
 
 .button-squashed .button {
     -webkit-transform: scaleX(0.7059);
+    -moz-transform: scaleX(0.7059);
 }
 
 .button-stretched .button {
     -webkit-transform: scaleX(1.417);
+    -moz-transform: scaleX(1.417);
 }
 
 .cell {
@@ -284,6 +305,10 @@ li {
         left bottom,
         color-stop(0, #0099FF),
         color-stop(1, #0066F2)
+    );
+    background-image: url(<?php echo data_encode('chevron_white.svg', 'image/svg+xml'); ?>), -moz-linear-gradient(
+        #0099FF,
+        #0066F2
     );
     background-size: 25px 13px, 100% 44px;
     background-position: center right, top left;
@@ -365,6 +390,7 @@ function back_image($mini, $mode) {
 
     border-width: 15px 5px 15px 14px;
     -webkit-border-image: <?php back_image(false, 0); ?>;
+    -moz-border-image: <?php back_image(false, 0); ?>;
 }
 
 #back-shadow {
@@ -373,7 +399,9 @@ function back_image($mini, $mode) {
     width: 104px;
     height: 60px;
     -webkit-transform: scale(0.5);
+    -moz-transform: scale(0.5);
     -webkit-transform-origin: top left;
+    -moz-transform-origin: top left;
 <?php } else { ?>
     width: 52px;
     height: 30px;
@@ -381,6 +409,7 @@ function back_image($mini, $mode) {
     left: -14px;
     top: -15px;
     -webkit-mask-box-image: <?php back_image(false, 1); ?>;
+    -moz-mask-box-image: <?php back_image(false, 1); ?>;
     
     z-index: -1;
 }
@@ -402,20 +431,50 @@ function back_image($mini, $mode) {
 
 .navbar-label, #back-button, .navigation-view-2-container, .navigation-view-1 {
     -webkit-transition-property: -webkit-transform, opacity;
+    -moz-transition-property: -moz-transform, opacity;
     -webkit-transition-timing-function: ease-in-out; /* iOS uses exactly this */
+    -moz-transition-timing-function: ease-in-out; /* iOS uses exactly this */
     -webkit-transition-duration: 0.35s;
+    -moz-transition-duration: 0.35s;
 }
 
 .freeze, .freeze .container, .freeze .navigation-view-1, .freeze .navigation-view-2-container, .freeze .navbar-label, .freeze #back-button {
     -webkit-transition-duration: 0s !important;
+    -moz-transition-duration: 0s !important;
 }
 
-       #first-label { -webkit-transform: translateX(0); opacity: 1; }
-.page2 #first-label { -webkit-transform: translateX(-100%); opacity: 0; }
-       #second-label { -webkit-transform: translateX(60%); opacity: 0; }
-.page2 #second-label { -webkit-transform: translateX(0); opacity: 1; }
-       #back-button { -webkit-transform: translateX(50%); opacity: 0; }
-.page2 #back-button { -webkit-transform: translateX(9px); opacity: 1; }
+#first-label { 
+       -webkit-transform: translateX(0);
+       -moz-transform: translateX(0);
+       opacity: 1; 
+}
+.page2 #first-label {
+    -webkit-transform: translateX(-100%);
+    -moz-transform: translateX(-100%);
+    opacity: 0; 
+}
+
+#second-label {
+    -webkit-transform: translateX(60%);
+    -moz-transform: translateX(60%);
+    opacity: 0; 
+}
+.page2 #second-label {
+    -webkit-transform: translateX(0);
+    -moz-transform: translateX(0);
+    opacity: 1;
+}
+       
+#back-button {
+    -webkit-transform: translateX(50%);
+    -moz-transform: translateX(50%);
+    opacity: 0;
+}
+.page2 #back-button {
+    -webkit-transform: translateX(9px);
+    -moz-transform: translateX(9px);
+    opacity: 1;
+}
 
 
 .navigation-view-container {
@@ -424,6 +483,7 @@ function back_image($mini, $mode) {
 
 .navigation-view-2-container {
     -webkit-transform: translateX(100%);
+    -moz-transform: translateX(100%);
     position: absolute;
     width: 100%;
     top: 0; left: 0;
@@ -431,6 +491,7 @@ function back_image($mini, $mode) {
 
 .page2 .navigation-view-2-container {
     -webkit-transform: translateX(0);
+    -moz-transform: translateX(0);
     position: relative;
 }
     
@@ -442,12 +503,14 @@ function back_image($mini, $mode) {
 
 .navigation-view-1 {
     -webkit-transform: translateX(0);
+    -moz-transform: translateX(0);
     display: block;
     position: relative;
 }
 
 .page2 .navigation-view-1 {
     -webkit-transform: translateX(-100%);
+    -moz-transform: translateX(-100%);
     position: absolute;
     top: 44px; left: 0;
 }
@@ -470,21 +533,34 @@ body {
         from(#656565),
         to(#959595)
     );
+    background-image: -moz-linear-gradient(
+        #656565,
+        #959595
+    );
 }
 
 .container {
+}
+
+.container {
+    <?php if ($device == 'computer') echo "-webkit-transform: translateY(0);" ?>
+    <?php if ($device == 'computer') echo "-moz-transform: translateY(0);" ?>
+    -webkit-transition-property: -webkit-transform;
+    -moz-transition-property: -moz-transform;
+    -webkit-transition-duration: 0.3s;
+    -moz-transition-duration: 0.3s;
+    -webkit-transition-timing-function: ease-in-out;
+    -moz-transition-timing-function: ease-in-out;
+    background-color: #e1e1e1;
+    -webkit-box-shadow: 0 0 50px black;
+    -moz-box-shadow: 0 0 50px black;
+    -webkit-border-radius: 15px;
+    -moz-border-radius: 15px;
     margin-left: 10%;
     margin-right: 10%;
     position: static;
     margin-top: 25%;
     margin-bottom: 25%;
-    <?php if ($device == 'computer') echo "-webkit-transform: translateY(0);" ?>
-    -webkit-transition-property: -webkit-transform;
-    -webkit-transition-duration: 0.3s;
-    -webkit-transition-timing-function: ease-in-out;
-    background-color: #e1e1e1;
-    -webkit-box-shadow: 0 0 50px black;
-    -webkit-border-radius: 15px;
 }
 
 .container-rounded {
@@ -520,6 +596,7 @@ body {
 
     overflow: hidden;
     -webkit-border-bottom-right-radius: 15px;
+    -moz-border-bottom-right-radius: 15px;
 }
 
 
@@ -541,7 +618,9 @@ body {
     margin-bottom: 50px;
     width: 190px;
     height: 190px;
-    -webkit-background-size: 100%;
+    -webkit-background-size: 100% 100%;
+    -moz-background-size: 100% 100%;
+    background-size: 100% 100%;
     margin-left: auto;
     margin-right: auto;
     background-image: url(holybejesus.png);
@@ -585,9 +664,15 @@ body {
         color-stop(0%, #a8abbb),
         color-stop(100%, #ffffff)
     );
+    background-image: -moz-linear-gradient(
+        #ffffff,
+        #a8abbb
+    );
 
     -webkit-border-top-left-radius: 15px;
+    -moz-border-top-left-radius: 15px;
     -webkit-border-top-right-radius: 15px;
+    -moz-border-top-right-radius: 15px;
 
     height: 43px;
     margin: 0;
@@ -638,7 +723,9 @@ body {
 
 .question-answer:last-child {
     -webkit-border-bottom-right-radius: 15px;
+    -moz-border-bottom-right-radius: 15px;
     -webkit-border-bottom-left-radius: 15px;
+    -moz-border-bottom-left-radius: 15px;
 }
 
 <?php } ?>
@@ -708,6 +795,7 @@ body {
 body {
     background-color: #c8cacc;
     -webkit-background-size: 101% 100px;
+    -moz-background-size: 101% 100px;
     background-repeat: no-repeat;
     background-image: -webkit-gradient(
         linear,
@@ -851,11 +939,13 @@ body {
 
     #back-button {
         -webkit-border-image: <?php back_image(true, 0); ?> !important;
+        -moz-border-image: <?php back_image(true, 0); ?> !important;
         border-width: 12px 4px 12px 10px;
     }
 
     #back-shadow {
         -webkit-mask-box-image: <?php back_image(true, 1); ?> !important;
+        -moz-mask-box-image: <?php back_image(true, 1); ?> !important;
     }
 
     #back-text {
