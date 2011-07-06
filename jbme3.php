98a9dd48ec2632f030c235936e6c8580abc31c5c
Grant Paul <chpwn@chpwn.com>
2011-07-05 22:17:23 -0700

    analytics, async

--- jbme3.php
+++ jbme3.php
@@ -1423,6 +1423,19 @@ $url = data_encode($pdf, 'application/pdf');
 echo "<script>pdf = '$url'</script>\n";
 }
 ?>
+
+<script type="text/javascript">
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-19159861-4']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+</script>
+
 </body>
 </html>
 <?php
